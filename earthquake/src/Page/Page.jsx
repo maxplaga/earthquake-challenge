b5c50d8c02466499067d3b5543ae443861446a95
Max Plaga <max.plaga@student.hpi.de>
2019-10-23 15:53:34 +0200

    added basic file structure

---
+++ earthquake/src/Page/Page.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "./Page.css";
+
+import DonationBox from "./DonationBox/DonationBox.jsx";
+
+export default class Page extends Component {
+    render() {
+        return (
+            <div className="eq-page">
+                <h1>Earthquake</h1>
+                <p>You can find all the latest earthquakes on this site.</p>
+                <DonationBox/>
+            </div>
+        );
+    }
+}
