38977377468675c22967b4f195edea0e731e211c
Max Plaga <max.plaga@student.hpi.de>
2019-10-26 18:30:12 +0200

    *added fetching of events in radius (hardcoded for now)

--- earthquake/src/Page/Map/Map.jsx
+++ earthquake/src/Page/Map/Map.jsx
@@ -9,9 +9,7 @@ export default class Map extends Component {
         ;
     }
 
-
     componentDidMount() {
-        console.log("cdm");
 
     }
 
@@ -27,28 +25,6 @@ export default class Map extends Component {
             alert("Geolocation is not supported by this browser.");
         }
     };
-
-    showLocation() {
-        console.log("show location");
-        console.log(this.state.geoLocation);
-        var latitude =  this.state.geoLocation.coords.latitude;
-        var longitude = this.state.geoLocation.coords.longitude;
-        var latlongvalue = latitude + "," + longitude;
-        var img_url =
-            "https://maps.googleapis.com/maps/api/staticmap?center="
-            + latlongvalue +
-            "&zoom=14&size=400x300&key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
-        document.getElementById("mapholder").innerHTML =
-            "<img src='"+img_url+"'>";
-    }
-
-    errorHandler(err) {
-        if(err.code === 1) {
-            alert("Error: Access is denied!");
-        } else if( err.code === 2) {
-            alert("Error: Position is unavailable!");
-        }
-    }
 
     getImg = () => {
         const latitude =  this.state.geoLocation.coords.latitude;
@@ -75,14 +51,15 @@ export default class Map extends Component {
                 <button type="button" className="btn btn-primary" onClick={this.setGeoLocation}>Locate me!</button>
 
                 {geoLocation &&
-                    <div>
-                        <p>Longitude: {geoLocation.coords.longitude}</p>
-                        <p>Latitude: {geoLocation.coords.latitude}</p>
-                        <img src={this.getImg()} alt={"Map with your location"}/>
+                    <div className="card" style={{ width: "18rem"}}>
+                        <img src={this.getImg()} className="card-img-top" alt="Map with your location"/>
+                            <div className="card-body">
+                                <h5 className="card-title">Your location</h5>
+                                <p className="card-text">This is where we located your browser. Please keep in mind that some inaccuracy is to be expected.</p>
+                                </div>
                     </div>
-
-
                 }
+
       </div>
         );
     }
--- earthquake/src/Page/Page.jsx
+++ earthquake/src/Page/Page.jsx
@@ -9,12 +9,18 @@ export default class Page extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            events: [],
+            latestEvents: [],
+            nearbyEvents: [],
         };
     }
 
 
     componentDidMount() {
+        this.getLatestEvents();
+        this.getNearbyEvents();
+    }
+
+    getLatestEvents = () => {
         const format = 'geojson';
         const startTime = '2019-01-01';
         const endTime = '2019-01-02';
@@ -22,21 +28,54 @@ export default class Page extends Component {
         fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=${format}&starttime=${startTime}&endtime=${endTime}&limit=${limit}`)
             .then((response) => response.json())
             .then((json) => {
-                console.log(json);
-                this.setState({events: json.features});
-                console.log(this.state.events)
+                this.setState({latestEvents: json.features});
             })
 
-    }
+    };
+
+    getNearbyEvents = () => {
+        const format = 'geojson';
+        const startTime = '2016-01-01';
+        const endTime = '2019-10-03';
+        const limit = '5';
+        const latitude = '52.403';
+        const longitude = '13.0626';
+        const maxradiuskm = '2000';
+
+        fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=${format}&starttime=${startTime}&endtime=${endTime}&limit=${limit}&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${maxradiuskm}`)
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState({nearbyEvents: json.features});
+            })
+
+    };
 
     render() {
+        const latestEvents = this.state.latestEvents;
+        const nearbyEvents = this.state.nearbyEvents;
+
         return (
             <div className="eq-page">
-                <h1>Earthquake</h1>
-                <p>You can find all the latest earthquakes on this site.</p>
-                <SearchBox/>
-                <EventList events={this.state.events}/>
-                <Map></Map>
+                <div className="container">
+                    <h1>Earthquake</h1>
+                    <p>You can find all the latest earthquakes on this site.</p>
+
+                    <div className="row">
+                        <h3>Most recent Events:</h3>
+                        <EventList events={latestEvents}/>
+                    </div>
+
+                    <SearchBox/>
+                    <div className="row">
+                        <div className="col">
+                            <h3>Events near you:</h3>
+                            <EventList events={nearbyEvents}/>
+                        </div>
+                        <div className="col">
+                            <Map></Map>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         );
